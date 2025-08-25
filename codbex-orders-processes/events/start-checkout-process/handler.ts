a9def33b36d64047240eac6039dd5cc24f11b08f
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-08-25 12:47:08 +0300

    refactor start process event

--- codbex-orders-processes/events/start-checkout-process/handler.ts
+++ codbex-orders-processes/events/start-checkout-process/handler.ts
@@ -1,20 +1,26 @@
-import { SalesOrderEntityEvent, SalesOrderUpdateEntityEvent } from 'codbex-orders/gen/codbex-orders/dao/SalesOrder/SalesOrderRepository';
+import { SalesOrderEntityEvent, SalesOrderUpdateEntityEvent, SalesOrderRepository } from 'codbex-orders/gen/codbex-orders/dao/SalesOrder/SalesOrderRepository';
 import { Process } from "sdk/bpm";
 
 export function onMessage(message: string) {
 
+    const salesOrderRepository = new SalesOrderRepository();
     const messageEvent: SalesOrderEntityEvent | SalesOrderUpdateEntityEvent = JSON.parse(message);
 
     if (messageEvent.operation == 'update') {
-        const entity = messageEvent.entity;
 
-        if (entity.Status == 1) {
-            Process.start('checkout-process', undefined, {
+        const entity = messageEvent.entity;
+        const salesOrder = salesOrderRepository.findById(entity.Id);
+
+        if (entity.Status == 1 && salesOrder.Process == undefined) {
+
+            const processId = Process.start('checkout-process', undefined, {
                 'orderId': entity.Id,
                 'status': "New",
                 'delivery': entity.SentMethod == 2
             });
 
+            salesOrder.Process = processId;
+            salesOrderRepository.update(salesOrder);
         }
     }
 }
