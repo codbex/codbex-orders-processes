adaf5bee22275288c4f263ca38374d5e1cc8dbd8
tivmof <tomyivanov06@gmail.com>
2025-08-27 15:55:55 +0000

    feat: Minor changes to configurations

---
+++ codbex-orders-processes/events/checkout-process/handler.ts
@@ -0,0 +1,37 @@
+import { SalesOrderEntityEvent, SalesOrderUpdateEntityEvent, SalesOrderRepository } from 'codbex-orders/gen/codbex-orders/dao/SalesOrder/SalesOrderRepository';
+import { Process } from "sdk/bpm";
+import { SalesOrderStatus } from '../../types/Types';
+
+
+export function onMessage(message: string) {
+    try {
+
+        const salesOrderRepository = new SalesOrderRepository();
+        const messageEvent: SalesOrderEntityEvent | SalesOrderUpdateEntityEvent = JSON.parse(message);
+
+        if (messageEvent.operation == 'update') {
+
+            const entity = messageEvent.entity;
+            const salesOrder = salesOrderRepository.findById(entity.Id);
+
+            if (entity.Status == SalesOrderStatus.New && salesOrder.Process == undefined) {
+
+                const processId = Process.start('checkout-process', undefined, {
+                    'orderId': entity.Id
+                });
+
+                salesOrder.Process = processId;
+                salesOrderRepository.update(salesOrder);
+            }
+            else {
+                if (entity.Status == SalesOrderStatus.Canceled) {
+                    console.log("Cancel entity: ", JSON.stringify(entity));
+                    Process.correlateMessageEvent(entity.Process ?? "1", "cancel-order", new Map<string, any>([["event", true]]));
+                }
+            }
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
--- codbex-orders-processes/tasks/checkout-process/send-reject-email.ts
+++ codbex-orders-processes/tasks/checkout-process/send-reject-email.ts
@@ -1,21 +1,21 @@
-import { Process } from "sdk/bpm";
-import { sendMail } from "codbex-orders-processes/utils/mail-utils";
+// import { Process } from "sdk/bpm";
+// import { sendMail } from "codbex-orders-processes/utils/mail-utils";
 
-const executionContext = Process.getExecutionContext();
+// const executionContext = Process.getExecutionContext();
 
-const reviewBody = executionContext.getVariable('reviewBody');
+// const reviewBody = executionContext.getVariable('reviewBody');
 
-const subject = `Отказана поръчка`;
+// const subject = `Отказана поръчка`;
 
-const content = `
-Уважаеми/а ${reviewBody.Customer.Name},
+// const content = `
+// Уважаеми/а ${reviewBody.Customer.Name},
 
-Благодарим Ви, че направихте поръчка при нас. За съжаление, трябва да Ви уведомим, че поръчката Ви с номер #${reviewBody.Order.Number} не можа да бъде обработена в момента. Разбираме, че това може да е разочароващо, и искрено се извиняваме за причиненото неудобство.
-Ако имате въпроси или желаете помощ при създаването на нова поръчка, не се колебайте да се свържете с нашия екип за поддръжка.
-Благодарим Ви за проявения интерес към ${reviewBody.Order.StoreName} и се надяваме скоро отново да Ви обслужим.
+// Благодарим Ви, че направихте поръчка при нас. За съжаление, трябва да Ви уведомим, че поръчката Ви с номер #${reviewBody.Order.Number} не можа да бъде обработена в момента. Разбираме, че това може да е разочароващо, и искрено се извиняваме за причиненото неудобство.
+// Ако имате въпроси или желаете помощ при създаването на нова поръчка, не се колебайте да се свържете с нашия екип за поддръжка.
+// Благодарим Ви за проявения интерес към ${reviewBody.Order.StoreName} и се надяваме скоро отново да Ви обслужим.
 
-С уважение,
-Екипът на ${reviewBody.Order.StoreName} 
-`;
+// С уважение,
+// Екипът на ${reviewBody.Order.StoreName} 
+// `;
 
 // sendMail(reviewBody.Customer.Email, subject, content);
